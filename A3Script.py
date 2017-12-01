90f1f3d06ba478cddc7779195d45a1f62850ca5e
arvnr <32990824+arvnr@users.noreply.github.com>
2017-12-01 10:14:02 -0400

    Update A3Script.py

--- A3Script.py
+++ A3Script.py
@@ -1,3 +1,4 @@
+#Taken from ECMM600 Lab 4 - Modify Tweets with Python and Twitter API by Colin Conrad
 #Twitter Profiler app. This is a simple script to configure the Twitter API
 
 import tweepy, time, csv
@@ -24,44 +25,23 @@ def get_profile(screen_name):
 
 # OBJECTIVE ONE: GET TWITTER USER ID
 s = get_profile("CitronResearch")
-print "Name: " + s.name
-print "id: " + s.id_str
+print "Name:" +s.name
+print "id:" +s.id_str
+
 def get_tweets(screen_name):
-    alltweets = []
     try:
-        #https://developer.twitter.com/en/docs/tweets/timelines/overview describes user_timeline
-        tweets = api.user_timeline(screen_name, count=3200)
-        print "tweets"
-        alltweets.extend(tweets)
-        oldest = alltweets[-1].id - 1
-        print oldest
-        print len(tweets)
-        while len(tweets) > 0:
-            tweets = api.user_timeline(screen_name, count=3200, max_id=oldest)
-            alltweets.extend(tweets)
-            oldest = alltweets[-1].id - 1
-            print "...Fetching Tweets" % (len(alltweets))
+        tweets = api.user_timeline(screen_name = screen_name,count = 20)
     except:
-        user_profile = "broken"
-    return alltweets
-       
-list1 = []
-t = get_tweets("CitronResearch")
-for tweet in t:
-    list1.append(tweet.retweet_count)
+        tweets = "broken"
+    return tweets
 
-for tweet in t:
-    if tweet.retweet_count == max(list1):
-        text1 = tweet.text
+# uses the function to query a Twitter user. Try s = get_profile("cd_conrad")
+profiles = ["Shopify", "CitronResearch"]
 
-with open ('A3Export.csv', 'wb') as outfile:
+with open ('arvinrA3.csv', 'wb') as outfile:
     writer = csv.writer(outfile)
     writer.writerow(["id","user","created_at","text"])
-    for tweet in t:
-        if "FTC" in tweet.text:
-            writer.writerow([tweet.id_str,tweet.user.screen_name,tweet.created_at,tweet.text.encode('unicode-escape')])
-    t3 = get_tweets("Shopify")
-    for tweet in t3:
-        if "citron" in tweet.text:
-            writer.writerow([tweet.id_str,tweet.user.screen_name,tweet.created_at,tweet.text.encode('unicode-escape')])
-print "Most Retweeted Tweet by Citron: \"" + text1 + " \" count of " +str(max(list1))
+    for profile in profiles:
+        t = get_tweets(profile)
+        for tweet in t:
+            writer.writerow([tweet.user.id_str,tweet.user.screen_name,tweet.created_at,tweet.text.encode('unicode-escape')])
